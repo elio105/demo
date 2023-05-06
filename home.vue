dd20c71cd04b6b966155abc1ad47779bf6f0d7fd
elio105 <1252239856@qq.com>
2023-05-06 15:40:05 +0800

    测试

---
+++ home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        hollo word
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'GittestHome',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
