9cf80e14f9e596fcfb146ffad1168b7c69334a1b
elio105 <1252239856@qq.com>
2023-05-12 17:32:22 +0800

    master上提交

---
+++ master.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        我是master
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DemoMaster',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
